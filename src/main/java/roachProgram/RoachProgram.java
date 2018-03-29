141e558d599f8ef9d0b1ce177a00450e49d1ee40
ZEHCARIAH <21700570@handong.edu>
2018-03-29 22:50:28 +0900

    Add a BugInfestation class

--- src/main/java/roachProgram/RoachProgram.java
+++ src/main/java/roachProgram/RoachProgram.java
@@ -6,5 +6,10 @@ public class RoachProgram {
 	final double One_Bug_volume =0.002;
 	final double GROWTH=0.95;
 	
+	public static void main(String[] args) {
+		RoachProgram my=new RoachProgram();
+		my.calculate();
+}
+	
 	
 }

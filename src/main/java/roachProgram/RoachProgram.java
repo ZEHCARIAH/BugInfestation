5e29a2e390b544e91c73bd1427dd38c91beb0785
ZEHCARIAH <21700570@handong.edu>
2018-03-29 22:52:33 +0900

    Add a main method, Add local variables

--- src/main/java/roachProgram/RoachProgram.java
+++ src/main/java/roachProgram/RoachProgram.java
@@ -10,6 +10,36 @@ public class RoachProgram {
 		RoachProgram my=new RoachProgram();
 		my.calculate();
 }
-	
-	
+public void calculate() {
+		
+		Scanner myScanner=new Scanner(System.in);
+		
+		System.out.println("Enter the total volume of your house");
+		System.out.println("in cubic feet: ");
+		double housevolume =myScanner.nextDouble();
+		System.out.println("Enter the estiamted number of\nroaches in your house: ");
+		double startPopulation =myScanner.nextDouble();
+		double population =startPopulation;
+		double totalBugvolume =population *One_Bug_volume;
+
+		int countweek=0;
+
+		while(totalBugvolume<housevolume)
+		{
+			double newBugs = population*GROWTH;
+			double newBugsVolume= newBugs*One_Bug_volume;
+			population=population+newBugs;
+			totalBugvolume= totalBugvolume+newBugsVolume;
+			countweek=countweek+1;
+		}
+
+		System.out.println("Staring with a roach population of "+(int)startPopulation);
+		System.out.println("and a house with a volume of "+housevolume+" cubic feet \nafter"+countweek+"weeks,");
+		System.out.println("the house will be filled with "+(int)population+" roaches.");
+		System.out.println("They will fill a volume of "+(int)totalBugvolume+" cubic feet.");
+		System.out.println("Beteer call Debugging Experts Inc");
+
+	}
+
 }
+	
